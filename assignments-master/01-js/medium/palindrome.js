bf28268b26ecdf94b88f70b227022a3759f4db08
ATRIKA SHOW <readeratrika08@gmail.com>
2023-12-23 23:39:22 +0530

    palindrome problem when letters are case sensitive

---
+++ assignments-master/01-js/medium/palindrome.js
@@ -0,0 +1,20 @@
+/*
+  Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
+  Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
+*/
+
+function isPalindrome(str) {
+  let n = str.length;
+  let i =0; let j=0;
+  while (i<n/2) {
+    let f = str.charCodeAt(i);
+    let l = str.charCodeAt(n-i-1);
+    if(f != l){
+      return false;
+    }
+    i++;
+  }
+  return true;
+}
+console.log(isPalindrome("nan"));
+module.exports = isPalindrome;
