99a8750b4d0abca43d3c1316a60eac0a91df8eb9
ATRIKA SHOW <readeratrika08@gmail.com>
2023-12-31 15:57:10 +0530

    class basic practice question 2

--- Complete Js/practice.js
+++ Complete Js/practice.js
@@ -1,5 +1,8 @@
 // You are creating a website for your college. Create a class User with 2 properties, name &email. It also has a method called viewData( ) that allows user to view website data.
+//Create a new class called Admin which inherits from User. Add a new method called editData to Admin that allows it to edit website data
 
+// make a random data
+let data = "College data";
 
 class user {
     constructor(name, email){
@@ -11,8 +14,21 @@ class user {
     }
 }
 
+class admin extends user{
+    editData(){
+        data = "data edited";
+        console.log(`this is new data => ${data}`);
+    }
+}
+
 // create object of user class
 let stu = new user("atrika", "a@gmail.com");
 console.log(stu.name);
 console.log(stu.email);
 console.log(stu.viewData());
+
+// create object of admin class
+let admin1 = new admin();
+
+console.log(admin1);    // name & email will be undefined as we have not passed 
+console.log(admin1.editData());
