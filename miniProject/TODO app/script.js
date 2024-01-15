2ff259ddacb8d0fa974b626af09b8126c8245aab
ATRIKA SHOW <readeratrika08@gmail.com>
2024-01-15 23:57:41 +0530

    remove text inside input box after adding a todo

--- miniProject/TODO app/script.js
+++ miniProject/TODO app/script.js
@@ -2,21 +2,26 @@
 let enterTaskInp = document.getElementById("enterTaskInp");
 let addTaskBtn = document.getElementById("addTaskBtn");
 
-let userText;
+let userText ="";
+// button should be disable if we don't have any text
+
 enterTaskInp.addEventListener("change", (e)=>{
     userText = enterTaskInp.value; 
-})
+    console.log(enterTaskInp.value);
+});
 
-addTaskBtn.addEventListener("click", ()=>{
-    console.log(userText);
+addTaskBtn.addEventListener("click", (e)=>{
     // each time when I click add task button, it will create a div & show userText inside div
 
     let eachTask = document.createElement("div");
-    eachTask.style.backgroundColor = "blue";
     eachTask.setAttribute("class", "eachTaskStyle");
     eachTask.innerText = userText;
 
     //select where will you append
     let taskBox = document.getElementById("taskBox");
     taskBox.append(eachTask);
+
+    // should remove all the text inside input box
+    enterTaskInp.value = "";
+    userText = "";      // must be clear else if we click button; the prev task will be added
 })
