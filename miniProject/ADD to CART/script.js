aacfaeba5994c5a78a1040631c47fb7ab51067a2
ATRIKA SHOW <readeratrika08@gmail.com>
2024-01-18 00:50:28 +0530

    added => add more product & remove product feature

--- miniProject/ADD to CART/script.js
+++ miniProject/ADD to CART/script.js
@@ -1,25 +1,62 @@
-console.log("okay");
 let cartBtn = document.getElementById("cartBtn");
 
-let i =0;
-cartBtn.addEventListener("click", ()=>{
+// to show the message, we should create a message
+let msg = document.createElement("p");
+msg.innerText = "Product is added";
+
+// Now there should a option to add more & remove from cart also
+// create a add more button
+let addMoreBtn = document.createElement("button");
+addMoreBtn.innerText = "+";
+
+// remove cart button
+let removeBtn = document.createElement("button");
+removeBtn.innerText = "-";
+
+cartBtn.addEventListener("click", () => {
     // after clicking cart button there will be 2 task
     // first => show the message
     // second => increase count in cart button
-
-    // to show the message, we should create a message
-    let msg = document.createElement("p");
-    msg.innerText = "Product is added";
 
     // where to append ( after image )
     let productImage = document.getElementById("productImage");
     productImage.after(msg);
 
     // update the count in cart
-    let count = document.getElementById("count");
-    let value = parseInt(count.innerText);   // we should parse it to int else it will be treated as string
-    count.innerText = value+1;
+    updateCount();
 
     // should hidden add to cart button
     cartBtn.style.visibility = "hidden";
+    
+    //append add more button
+    cartBtn.before(addMoreBtn);
+    cartBtn.before(removeBtn);
+
+})
+
+function updateCount() {
+    let count = document.getElementById("count");
+    let value = parseInt(count.innerText);   // we should parse it to int else it will be treated as string
+    count.innerText = value + 1;
+}
+
+function decCount() {
+    let count = document.getElementById("count");
+    let value = parseInt(count.innerText);   // we should parse it to int else it will be treated as string
+    if (value == 1) {
+        cartBtn.style.visibility = "visible";
+        // we should remove + & - button & message also
+        addMoreBtn.remove();
+        removeBtn.remove();
+        msg.remove();
+    }
+    count.innerText = value - 1;
+}
+
+// when we will click addMoreBtn, it will increase count
+addMoreBtn.addEventListener("click", () => {
+    updateCount();
+})
+removeBtn.addEventListener("click", () => {
+    decCount();
 })
